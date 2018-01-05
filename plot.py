0a2743848d478bc2a31719706c12eb8b7105ec90
Michael Egorov <michwill@gmail.com>
2018-01-04 16:33:33 -0800

    Plot number of users

---
+++ plot.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import pylab
+from datetime import datetime
+import numpy as np
+import matplotlib.dates as mdates
+
+
+def load():
+    names = [f for f in os.listdir() if f.endswith('.json')]
+    times = [datetime.fromtimestamp(os.path.getmtime(f)) for f in names]
+    N = []
+    for name in names:
+        with open(name) as f:
+            N.append(len(json.load(f)))
+    times = np.array(times)
+    N = np.array(N)
+    order = times.argsort()
+    return times[order], N[order]
+
+
+if __name__ == '__main__':
+    fmt = mdates.DateFormatter('%m/%d')
+    ax = pylab.axes()
+    ax.xaxis.set_major_formatter(fmt)
+    times, N = load()
+    pylab.plot(times, N, marker='o', linewidth=2)
+    pylab.show()
