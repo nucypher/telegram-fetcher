72bfef942277e4818de4b6a7af7e814d5aff9370
Michael Egorov <michwill@gmail.com>
2018-01-30 13:14:27 -0800

    Retry on abort

--- fetcher-ng.py
+++ fetcher-ng.py
@@ -19,54 +19,60 @@ def json_serial(obj):
 
 
 LIMIT = 500
-
-client = TelegramClient('fetcher-session', config.api_id, config.api_hash)
-client.connect()
-
-if not client.is_user_authorized():
-    client.sign_in(phone=config.phone)
-    me = client.sign_in(code=int(input('Enter code: ')))
-
-channel = client(ResolveUsernameRequest(config.channel)).chats[0]
-
-total, messages, senders = client.get_message_history(
-        channel, limit=1, offset_id=0)
-cursor = messages[0].id + 1
+output_file = 'nucypher_telegram.json'
 users = []
 
-
-def check_participant(u):
-    try:
-        client(GetParticipantRequest(channel, u))
-        return True
-    except UserNotParticipantError:
-        return False
-
-
 while True:
-    total, messages, senders = client.get_message_history(
-            channel, limit=LIMIT, offset_id=cursor)
-    if not messages:
-        break
+    try:
+        client = TelegramClient('fetcher-session', config.api_id, config.api_hash)
+        client.connect()
 
-    for m, s in zip(messages, senders):
-        if isinstance(m, types.MessageService):
-            if isinstance(m.action, types.MessageActionChatJoinedByLink):
-                if check_participant(s):
-                    users.append((m.date, s.to_dict()))
-            elif isinstance(m.action, types.MessageActionChatAddUser):
-                for u_id in m.action.users:
-                    if u_id == s.id:
-                        user = s
-                    else:
-                        user = client.get_entity(u_id)
-                    if check_participant(s):
-                        users.append((m.date, user.to_dict()))
+        if not client.is_user_authorized():
+            client.sign_in(phone=config.phone)
+            me = client.sign_in(code=int(input('Enter code: ')))
 
-    cursor = messages[-1].id
-    print(cursor)
-    if cursor <= 1:
-        break
+        channel = client(ResolveUsernameRequest(config.channel)).chats[0]
+
+        total, messages, senders = client.get_message_history(
+                channel, limit=1, offset_id=0)
+        cursor = messages[0].id + 1
+
+        def check_participant(u):
+            try:
+                client(GetParticipantRequest(channel, u))
+                return True
+            except UserNotParticipantError:
+                return False
+
+        while True:
+            total, messages, senders = client.get_message_history(
+                    channel, limit=LIMIT, offset_id=cursor)
+            if not messages:
+                break
+
+            for m, s in zip(messages, senders):
+                if isinstance(m, types.MessageService):
+                    if isinstance(m.action, types.MessageActionChatJoinedByLink):
+                        if check_participant(s):
+                            users.append((m.date, s.to_dict()))
+                    elif isinstance(m.action, types.MessageActionChatAddUser):
+                        for u_id in m.action.users:
+                            if u_id == s.id:
+                                user = s
+                            else:
+                                user = client.get_entity(u_id)
+                            if check_participant(s):
+                                users.append((m.date, user.to_dict()))
+
+            cursor = messages[-1].id
+            print(cursor)
+            if cursor <= 1:
+                break
+
+    except ConnectionAbortedError:
+        print('Connection was aborted')
+
+    break
 
 # forward in time
 # + filter repetitions
@@ -79,6 +85,5 @@ for d, u in in_users:
         users.append((d, u))
 
 print('Fetched %s users' % len(users_added))
-output_file = '%s-dated-%s.json' % (config.channel, len(users_added))
 with open(output_file, 'w') as f:
     json.dump(users, f, default=json_serial, indent=4)
